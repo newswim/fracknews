5d54fc467c0f4aa50e0289c771509baae7e76f57
Dan Minshew <ofenixculpa@gmail.com>
2015-07-22 23:42:48 -0500

    Update fracknews.js

--- fracknews.js
+++ fracknews.js
@@ -7,3 +7,8 @@ if (Meteor.isServer) {
     // code to run on server at startup
   });
 }
+
+
+// Let's try and come up with something a little less on the nose?
+/* ## Mandate
+### Over the past two decades, the Oil and Gas industry has systematically ingratiated itself to US lawmakers and passed cover-of-night legislation that safeguards highly toxifying practices. FrackNews aspires to track news articles and scholarly studies which highlight the surrounding political climate and allow an opportunity for open and civilized discussion. */
